106f5d23bce35b6ea2c833e843112ae0ef2fdb03
Jay <jlawhorn@mit.edu>
2015-02-26 16:07:47 +0100

    merge from xinmei

--- Analyzer/HLTv2.cc
+++ Analyzer/HLTv2.cc
@@ -23,9 +23,9 @@ void HLTv2::apply(const std::vector<double> & inputCharge, const std::vector<dou
   std::vector<double> corrCharge;
   fPedestalSubFxn_.Calculate(inputCharge, inputPedestal, corrCharge);
   
-  Float_t tsShift3=-HcalTimeSlew::delay(corrCharge[3],HcalTimeSlew::MCShift,fTimeSlewBias); 
-  Float_t tsShift4=-HcalTimeSlew::delay(corrCharge[4],HcalTimeSlew::MCShift,fTimeSlewBias); 
-  Float_t tsShift5=-HcalTimeSlew::delay(corrCharge[5],HcalTimeSlew::MCShift,fTimeSlewBias); 
+  Float_t tsShift3=HcalTimeSlew::delay(inputCharge[3]-inputPedestal[3],HcalTimeSlew::MC,fTimeSlewBias); 
+  Float_t tsShift4=HcalTimeSlew::delay(inputCharge[4]-inputPedestal[4],HcalTimeSlew::MC,fTimeSlewBias); 
+  Float_t tsShift5=HcalTimeSlew::delay(inputCharge[5]-inputPedestal[5],HcalTimeSlew::MC,fTimeSlewBias); 
 
   Float_t i3=0;
   getLandauFrac(tsShift3,tsShift3+25,i3);
@@ -53,10 +53,10 @@ void HLTv2::apply(const std::vector<double> & inputCharge, const std::vector<dou
   }
   
   if (ch5<-3 && fNegStrat==HLTv2::ReqPos) {
-    std::cout << "original ch4: " << ch4 << ", ch5: " << ch5 << std::endl;
+    //std::cout << "original ch4: " << ch4 << ", ch5: " << ch5 << std::endl;
     ch4=ch4+(ch5+3);
     ch5=-3;
-    std::cout << "new ch4: " << ch4 << ", ch5: " << ch5 << std::endl;
+    //std::cout << "new ch4: " << ch4 << ", ch5: " << ch5 << std::endl;
   }
 
   HLTOutput.clear();
@@ -101,6 +101,16 @@ void HLTv2::applyXM(const std::vector<double> & inputCharge, const std::vector<d
 //Landau turn on by default at left edge of time slice 
 // normalized to 1 on [0,10000]
 void HLTv2::getLandauFrac(Float_t tStart, Float_t tEnd, Float_t &sum) const{
+
+  Float_t landauFrac[125] = {0, 7.6377e-05, 0.000418655, 0.00153692, 0.00436844, 0.0102076, 0.0204177, 0.0360559, 0.057596, 0.0848493, 0.117069, 0.153152, 0.191858, 0.23198, 0.272461, 0.312438, 0.351262, 0.388476, 0.423788, 0.457036, 0.488159, 0.517167, 0.54412, 0.569112, 0.592254, 0.613668, 0.633402, 0.651391, 0.667242, 0.680131, 0.688868, 0.692188, 0.689122, 0.67928, 0.662924, 0.64087, 0.614282, 0.584457, 0.552651, 0.51997, 0.487317, 0.455378, 0.424647, 0.395445, 0.367963, 0.342288, 0.318433, 0.29636, 0.275994, 0.257243, 0.24, 0.224155, 0.2096, 0.196227, 0.183937, 0.172635, 0.162232, 0.15265, 0.143813, 0.135656, 0.128117, 0.12114, 0.114677, 0.108681, 0.103113, 0.0979354, 0.0931145, 0.0886206, 0.0844264, 0.0805074, 0.0768411, 0.0734075, 0.0701881, 0.0671664, 0.0643271, 0.0616564, 0.0591418, 0.0567718, 0.054536, 0.0524247, 0.0504292, 0.0485414, 0.046754, 0.0450602, 0.0434538, 0.041929, 0.0404806, 0.0391037, 0.0377937, 0.0365465, 0.0353583, 0.0342255, 0.0331447, 0.032113, 0.0311274, 0.0301854, 0.0292843, 0.0284221, 0.0275964, 0.0268053, 0.0253052, 0.0238536, 0.0224483, 0.0210872, 0.0197684, 0.0184899, 0.01725, 0.0160471, 0.0148795, 0.0137457, 0.0126445, 0.0115743, 0.0105341, 0.00952249, 0.00853844, 0.00758086, 0.00664871,0.00574103, 0.00485689, 0.00399541, 0.00315576, 0.00233713, 0.00153878, 0.000759962, 0 };
+
+  if (abs(tStart-tEnd)!=25) {
+    sum=0;
+    return;
+  }
+  sum=landauFrac[int(ceil(tStart+25))];
+  return;
+  /*
   // can be further optimized to reduce computational time
   if (tStart==0 && tEnd==25) {
     sum=0.613668;
@@ -119,7 +129,7 @@ void HLTv2::getLandauFrac(Float_t tStart, Float_t tEnd, Float_t &sum) const{
     else sum+=landauFrac[i];
   }
   return;
-
+  */
 }
 
 void HLTv2::PulseFraction(Double_t fC, Double_t *TS46) const{
